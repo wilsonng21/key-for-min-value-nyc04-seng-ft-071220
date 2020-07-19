cac92944f74eb8fc88f587e6b955511510a8d43f
Wilson Ng <wilsongch92@gmail.com>
2020-07-19 23:09:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    nums_array = []
+    new_array = []
+    this_key = nil
+    name_hash.each do |name, value|
+        nums_array << value
+        new_array = nums_array.sort
+        if new_array[0] == value 
+            this_key = name 
+        end
+    end
+    p new_array
+    p this_key
 end
+
